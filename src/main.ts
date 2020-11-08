b120cdbfae1bbb3465afe427d35b32afac1ccedc
Amir <hesamian@uwm.edu>
2020-11-08 15:20:43 -0600

    More improvement with validations

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import validateEnv from './utils/validateEnv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // validateEnv();
-
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
     .setTitle('Nest.js API')
