ed66ad1991a438903df9ae3af4502644acfc3567
Amir <hesamian@uwm.edu>
2020-11-08 13:02:46 -0600

    More improvement with configs

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           entities: [Token, User],
           synchronize: true,
           migrationsRun: true,
-          logging: true,
+          logging: false,
         };
 
         if (configService.get<string>('ENV') === 'Development') {
