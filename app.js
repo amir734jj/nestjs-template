056badc40c5e3cca6418a17e237d6e30ef8557ed
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-11-14 22:54:53 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "ts-webapp-boilerplate",
+  "description": "A barebones Node.js app using Nest.js",
+  "repository": "https://github.com/amir734jj/ts-webapp-boilerplate",
+  "keywords": ["node", "nest.js", "typescript"]
+}
